32865e72dcf18d9f1ea1769849df7b6bfefd30db
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-05-12 17:35:10 +0200

    feat: click away no modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,17 +1,42 @@
 import {twa} from "../../utils/twa";
-import {PropsWithChildren} from "react";
+import {PropsWithChildren, useEffect, useRef, useState} from "react";
 
-
-interface ModalProps {
-    opened: boolean;
+export interface ModalContentProps{
     onClose: () => void;
 }
 
+export interface ModalProps extends ModalContentProps{
+    opened: boolean;
+}
+
 export function Modal(props: PropsWithChildren<ModalProps>){
-    if(!props.opened) return null;
+    const [opened, setOpened] = useState(props.opened);
+    useEffect(() => {
+        setOpened(props.opened);
+    }, [props.opened]);
+
+    if(!opened) return null;
+
+    return <ModalContent onClose={props.onClose}>{props.children}</ModalContent>;
+}
+
+function ModalContent(props: PropsWithChildren<ModalContentProps>){
+    const modalRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        document.addEventListener('click', handleRefClick);
+        return () => {document.removeEventListener('click', handleRefClick)};
+    },[]);
+
+    const handleRefClick = (event: MouseEvent) => {
+        if(!modalRef.current?.contains(event.target as Element)){
+            props.onClose();
+        }
+    };
+
     return (
         <div className={twa`h-screen w-screen fixed top-0 left-0 flex bg-[#00000080]`}>
-            <div className={twa`relative m-auto bg-gray-200 rounded-md shadow-lg px-4 py-3`}>
+            <div ref={modalRef} className={twa`relative m-auto bg-gray-200 rounded-md shadow-lg px-4 py-3`}>
                 <div onClick={props.onClose} className={twa`absolute -top-[1rem] -right-[1rem] bg-white hover:bg-gray-300 rounded-full shadow-md cursor-pointer p-1`}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                          stroke="currentColor">
