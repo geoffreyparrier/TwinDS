9aea874d8d572df8a59d49d22ae510320b8b2b08
MartinPELCAT <martinpelcat@hotmail.com>
2022-05-12 12:36:04 +0200

    add avatar , avatargroup, badge

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 import "twind/colors";
 export { default as Button } from "./Button";
 export { Label } from "./Label/Label";
+export { Avatar } from "./Avatar/Avatar";
+export { AvatarGroup } from "./AvatarGroup/AvatarGroup";
+export { Badge } from "./Badge/Badge";
