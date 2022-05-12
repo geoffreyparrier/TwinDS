54f9b05ae52b33449f0a0f8ffb646246217d38fd
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-05-12 14:32:51 +0200

    feat: loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,13 +1,20 @@
 import { twa } from "../../utils/twa";
 
 export interface LoaderProps {
-
+    size?: number;
+    borderSize?: number;
 }
 
-export function Loader(props: LoaderProps){
+export function Loader({size = 1, borderSize = 2}: LoaderProps){
     return (
-        <div className={twa``}>
-            loader
+        <div style={{borderTop: `solid ${borderSize}px #3D8EA8`}} className={twa`w-[${size}rem] h-[${size}rem] border-[${borderSize}px] border-gray-200 rounded-full animate-spin`}/>
+    );
+}
+
+export function BackdropLoader({size = 3, borderSize = 3}: LoaderProps){
+    return (
+        <div className={twa`w-screen h-screen fixed top-0 left-0 flex justify-center items-center text-3xl bg-[#00000080]`}>
+            <Loader size={size} borderSize={borderSize}/>
         </div>
     );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 import "twind/colors";
 export { Button } from "./Button/Button";
 export { Label } from "./Label/Label";
-export { Loader } from "./Loader/Loader";
+export { Loader, BackdropLoader } from "./Loader/Loader";
