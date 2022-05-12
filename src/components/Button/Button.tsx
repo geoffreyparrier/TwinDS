43e774a7fe13f361c71646c7ff3691ae99ced77a
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-05-12 14:43:53 +0200

    feat: loading button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import { twa } from "../../utils/twa";
 import {ReactNode} from "react";
+import {Loader} from "..";
 
 export interface ButtonProps extends Partial<JSX.IntrinsicElements["button"]>{
   label: string;
@@ -10,25 +11,29 @@ export interface ButtonProps extends Partial<JSX.IntrinsicElements["button"]>{
 }
 
 export const Button = (props: ButtonProps) => {
-    const {icon, label, iconPosition, filled, ...other} = props;
+    const {icon, label, iconPosition, filled, loading, ...other} = props;
     let colorClasses = 'not-disabled:hover:bg-main-700 not-disabled:hover:text-white text-main-800 border-main-700';
     if(props.filled) colorClasses = 'not-disabled:hover:bg-transparent not-disabled:hover:text-main-800 bg-main-700 text-white border-main-700';
     return (
         <button {...other} className={twa`flex gap-2 content-around border ${colorClasses} focus:outline-none px-4 py-2 rounded-full duration-150 transition outline-none active:scale-95 disabled:active:scale-100 disabled:opacity-50 disabled:cursor-default`}>
-            {(props.icon && (!props.iconPosition || props.iconPosition === 'left')) && (
-                <div className={twa``}>
-                    {props.icon}
+
+            {(!props.iconPosition || props.iconPosition === 'left') && (
+                <div className="my-auto">
+                    {loading ? (
+                        <Loader/>
+                    ) : props.icon}
                 </div>
             )}
             <div className={twa``}>
                 {props.label}
             </div>
-            {(props.icon && (props.iconPosition && props.iconPosition === 'right')) && (
-                <div className={twa``}>
-                    {props.icon}
+            {(props.iconPosition && props.iconPosition === 'right') && (
+                <div className="my-auto">
+                    {loading ? (
+                        <Loader/>
+                    ) : props.icon}
                 </div>
             )}
         </button>
     );
 };
-
