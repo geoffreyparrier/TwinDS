6b6a57b28265369db9dd74d349c3126d1649bf14
Antonin <a.caillon@openbee.com>
2022-05-12 15:36:42 +0200

    finalisation du radio boutton

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -20,7 +20,7 @@ export const RadioButton = (props: RadioButtonProps) => {
 
     return <div>
                 <input type="radio" id={props.id} name={props.name} value={props.value} checked={selected} 
-                onChange={() => onSelectClick()} className="h-4 w-4 relative top-0.5 bg-main-700"/>
+                onChange={() => onSelectClick()} className="h-4 w-4 relative top-0.5"/>
                 <label htmlFor={props.id} className="pl-2 pr-2">
                     {props.label}
                 </label> 
