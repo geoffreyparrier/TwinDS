e446b76dcdf9a2995d42ae4b35ac125246fad1c9
MartinPELCAT <martinpelcat@hotmail.com>
2022-05-12 15:26:30 +0200

    fuck it

--- src/components/Accordion/index.ts
+++ src/components/Accordion/index.ts
@@ -1,3 +1,4 @@
 import { Accordion as Accord } from "./Accordion";
+import { Pannel } from "./Pannel";
 
-export const Accordion = Object.assign(Accord, {});
+export const Accordion = Object.assign(Accord, { Pannel });
