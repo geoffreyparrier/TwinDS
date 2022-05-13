09fd5f6362789bac26cc308adcbd8cd62b0ae27e
MartinPELCAT <martinpelcat@hotmail.com>
2022-05-13 17:15:21 +0200

    finis speed dial button

--- src/components/SpeedDial/SpeedDialItem.tsx
+++ src/components/SpeedDial/SpeedDialItem.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { PlusIcon } from "../../icons/plus.icon";
+import { Button } from "../Button/Button";
 
-export const SpeedDialItem = () => {
-  return <div>SpeedDialItem</div>;
+type Props = {
+  icon?: React.ReactNode;
+  onClick?: () => void;
+};
+
+export const SpeedDialItem = ({ icon, onClick }: Props) => {
+  return <Button icon={icon ?? <PlusIcon />} onClick={onClick} />;
 };
