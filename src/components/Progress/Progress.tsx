dfeda2095886a62fa22d02497a258f58b27bf431
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-05-13 11:51:17 +0200

    feat: add custom height to progress

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -4,12 +4,13 @@ import {twa} from "../../utils/twa";
 interface ProgressProps{
     max?:number|string;
     value: number|string;
-    classes?: string
+    classes?: string,
+    height?: string;
 }
 
 export function Progress(props:ProgressProps){
-    const {max = 100, value, classes = ''} = props;
+    const {max = 100, value, classes = '', height = '0.5rem'} = props;
     return (
-        <progress id="progress-bar" className={twa`w-full h-2 block ${classes}`} max={max} value={value}> {value}% </progress>
+        <progress id="progress-bar" className={twa`w-full h-[${height}] block ${classes}`} max={max} value={value}> {value}% </progress>
     );
 }
