2420c6e6e3b03f90d29b26a0ad5f348881fedc71
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-05-13 10:11:52 +0200

    feat: Alert component

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,4 +9,5 @@ export { BackdropLoader, Loader } from "./Loader/Loader";
 export { RadioButton } from "./RadioButton/RadioButton";
 export { Modal } from "./Modal/Modal";
 export { Select } from "./Select";
+export { Alert } from "./Alert/Alert";
 
